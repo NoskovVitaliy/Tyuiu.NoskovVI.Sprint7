8780a66c828ad03428d9c5b44ccae6ef9345b83e
NoskovVitaliy <Tyuiu.NoskovVI@outlook.com>
2024-12-20 17:50:26 +0500

    Minor fixes

--- Tyuiu.NoskovVI.Sprint7.Project.V8/FormGuide.Designer.cs
+++ Tyuiu.NoskovVI.Sprint7.Project.V8/FormGuide.Designer.cs
@@ -36,9 +36,9 @@
             // labelGuide_NVI
             // 
             labelGuide_NVI.AutoSize = true;
-            labelGuide_NVI.Location = new Point(3, 50);
+            labelGuide_NVI.Location = new Point(3, 30);
             labelGuide_NVI.Name = "labelGuide_NVI";
-            labelGuide_NVI.Size = new Size(790, 300);
+            labelGuide_NVI.Size = new Size(790, 340);
             labelGuide_NVI.TabIndex = 0;
             labelGuide_NVI.Text = resources.GetString("labelGuide_NVI.Text");
             // 
